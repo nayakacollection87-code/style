ec0dc768e21f606d6162937307f706f002c620fc
nayakacollection87-code <nayakacollection87@gmail.com>
2026-05-29 18:14:48 +0700

    Create lib/utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,32 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+  }).format(value);
+}
+
+export function formatDate(date: string | Date): string {
+  return new Intl.DateTimeFormat('id-ID', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date(date));
+}
+
+export function generateReferralCode(userId: string): string {
+  return `NEX_${userId.substring(0, 8).toUpperCase()}_${Math.random().toString(36).substring(2, 8).toUpperCase()}`;
+}
+
+export function calculateCoinCost(views: number): number {
+  return Math.ceil(views / 1000);
+}
